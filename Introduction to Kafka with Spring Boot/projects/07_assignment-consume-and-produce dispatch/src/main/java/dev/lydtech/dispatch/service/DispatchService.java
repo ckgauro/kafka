adb60ae7310b50689c280161895df6b77f8a4022
ckgauro <ckgauro@gmail.com>
2026-03-22 21:03:10 -0400

    Section 06

---
+++ Introduction to Kafka with Spring Boot/projects/07_assignment-consume-and-produce dispatch/src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -0,0 +1,30 @@
+package dev.lydtech.dispatch.service;
+
+import dev.lydtech.dispatch.message.DispatchPreparing;
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.message.OrderDispatched;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DispatchService {
+
+    private static final String DISPATCH_TRACKING_TOPIC="dispatch.tracking";
+    private static final String ORDER_DISPATCHED_TOPIC="order.dispatched";
+    private final KafkaTemplate<String,Object> kafkaProducer;
+
+    public void process(OrderCreated orderCreated) throws Exception{
+        DispatchPreparing dispatchPreparing= DispatchPreparing.builder()
+                .orderId(orderCreated.getOrderId())
+                .build();
+        kafkaProducer.send(DISPATCH_TRACKING_TOPIC,dispatchPreparing).get();
+
+        OrderDispatched orderDispatched= OrderDispatched.builder()
+                .orderId(orderCreated.getOrderId())
+                .build();
+        kafkaProducer.send(ORDER_DISPATCHED_TOPIC,orderDispatched).get();
+
+    }
+}
